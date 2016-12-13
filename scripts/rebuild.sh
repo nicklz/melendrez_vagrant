a935e724d5ee00da00c4c514afa992823d97287a
Nick Kuhn <nicklz22@yahoo.com>
2016-12-13 14:25:12 -0800

    fixing database

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -4,6 +4,9 @@
 echo "drop database melendrez;" | mysql -uroot
 echo "create database melendrez;" | mysql -uroot
 
-mysql -u root -p melendrez --password="" < /home/vagrant/www/docroot/dump.sql
+mysql -u root -p melendrez --password="" < /home/vagrant/www/dump.sql
+
+cd ~/www/docroot/sites/local.melendrez.com && drush -u 1 sql-query "INSERT INTO users (name, pass, mail, theme, signature, language, init, timezone) VALUES ('', '', '', '', '', '', '', '')" && drush -u 1 sql-query "UPDATE users SET uid = 0 WHERE name = ''" && drush cc all
+
 
 cd ~/www/docroot/sites/local.melendrez.com && drush en melendrez_updates node_export_features_ui context_ui -y && drush updb -y &&  drush upwd --password='password' admin
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,4 +5,6 @@
 echo "drop database melendrez;" | mysql -uroot
 echo "create database melendrez;" | mysql -uroot
 
-mysql -u root -p melendrez --password="" < /home/vagrant/www/docroot/dump.sql
+mysql -u root -p melendrez --password="" < /home/vagrant/www/dump.sql
+
+cd ~/www/docroot/sites/local.melendrez.com && drush -u 1 sql-query "INSERT INTO users (name, pass, mail, theme, signature, language, init, timezone) VALUES ('', '', '', '', '', '', '', '')" && drush -u 1 sql-query "UPDATE users SET uid = 0 WHERE name = ''" && drush cc all

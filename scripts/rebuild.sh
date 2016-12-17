543ab023ec38b0a324318a2fb8863993dfc87efb
Nick Kuhn <nicklz22@yahoo.com>
2016-12-17 12:07:46 -0800

    fixes

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -6,7 +6,7 @@ echo "create database melendrez;" | mysql -uroot
 
 mysql -u root -p melendrez --password="" < /home/vagrant/www/dump.sql
 
-cd ~/www/docroot/sites/local.melendrez.com && rush -u 1 sql-query "INSERT INTO users (name, pass, mail, theme, signature, language, init, timezone) VALUES ('', '', '', '', '', '', '', '')" && drush -u 1 sql-query "UPDATE users SET uid = 0 WHERE name = ''" &&  drush dis overlay -y && drush cc all 
+cd ~/www/docroot/sites/local.melendrez.com && drush -u 1 sql-query "INSERT INTO users (name, pass, mail, theme, signature, language, init, timezone) VALUES ('', '', '', '', '', '', '', '')" && drush -u 1 sql-query "UPDATE users SET uid = 0 WHERE name = ''" &&  drush dis overlay -y && drush cc all 
 
 
-cd ~/www/docroot/sites/local.melendrez.com && drush en melendrez_updates  -y && drush fra -y &&  drush cc all
+cd ~/www/docroot/sites/local.melendrez.com && drush en melendrez_updates  -y && drush updb -y  &&  drush cc all
